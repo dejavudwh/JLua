9d8bfb1255e14f5527e3f59787dbd7dc81482d04
dejavudwh <494319000@qq.com>
2019-10-01 14:20:34 +0800

    Updated the decoding of the instruction

---
+++ src/vm/Instruction.java
@@ -0,0 +1,38 @@
+package vm;
+
+public class Instruction {
+
+    public static final int MAXARG_Bx = (1 << 18) - 1;// sBx 18 bit; 2^18 - 1
+    public static final int MAXARG_sBx = MAXARG_Bx >> 1;//MAXARG_Bx / 2
+
+    public static OpCode getOpCode(int i) {
+        // low 6 bit
+        return OpCode.values()[i & 0x3F];
+    }
+
+    public static int getA(int i) {
+        // >> 6 low 8 bit
+        return (i >> 6) & 0xFF;
+    }
+
+    public static int getC(int i) {
+        // >> 14 low 9 bit
+        return (i >> 14) & 0x1FF;
+    }
+
+    public static int getB(int i) {
+        return (i >> 23) & 0x1FF;
+    }
+
+    public static int getBx(int i) {
+        return (i >>> 14);
+    }
+
+    public static int getSBx(int i) {
+        return getBx(i) - MAXARG_sBx;
+    }
+
+    public static int getAx(int i) {
+        return i >>> 6;
+    }
+}

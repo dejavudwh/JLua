89c628a63d28208f73f2124a752d7ef41508dcb5
dejavudwh <494319000@qq.com>
2019-10-18 14:31:56 +0800

    Implement the CharSeq class that inherits the source code

--- src/compiler/lexer/CharSeq.java
+++ src/compiler/lexer/CharSeq.java
@@ -1,5 +1,8 @@
 package compiler.lexer;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CharSeq implements CharSequence {
 
     private String str;
@@ -9,18 +12,44 @@ public class CharSeq implements CharSequence {
         this.str = str;
     }
 
+    char nextChar() {
+        return str.charAt(pos++);
+    }
+
+    void next(int n) {
+        pos += n;
+    }
+
+    boolean startsWith(String prefix) {
+        return str.startsWith(prefix, pos);
+    }
+
+    int indexOf(String s) {
+        return str.indexOf(s, pos) - pos;
+    }
+
+    String substring(int beginIndex, int endIndex) {
+        return str.substring(beginIndex + pos, endIndex + pos);
+    }
+
+    String find(Pattern pattern) {
+        Matcher matcher = pattern.matcher(this);
+        return matcher.find()
+                ? matcher.group(0) : null;
+    }
+
     @Override
     public int length() {
-        return 0;
+        return str.length() - pos;
     }
 
     @Override
     public char charAt(int i) {
-        return 0;
+        return str.charAt(i + pos);
     }
 
     @Override
-    public CharSequence subSequence(int i, int i1) {
-        return null;
+    public CharSequence subSequence(int start, int end) {
+        return str.subSequence(start + pos, end + pos);
     }
 }
